b4a55abea030c93164a5c10f0de27e5c6c3a2b75
majocast <marcjgcastro@gmail.com>
2023-08-30 01:27:53 -0700

    added menu to nav for mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,44 +2,37 @@ import React, { useEffect } from 'react';
 import { Link as ScrollLink } from 'react-scroll';
 import { motion } from 'framer-motion';
 
-const slideDownAnimationVariants = {
-  initial: {
-    opacity: 0,
-    y: -50,
-  },
-  animate: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      delay: 0.5,
-    }
-  },
-}
-
 const NavBar = () => {
   const contents = ['about', 'portfolio', 'resume', 'contact'];
   
   useEffect(() => {
-    if(window.innerWidth > 768) {
-      document.getElementById('navConts').classList.add('flex space-x-4');
+    if(window.innerWidth >= 768) {
+      document.getElementById('navConts').classList.add('flex');
+      document.getElementById('navConts').classList.add('space-x-4');
+      document.getElementById('menuButton').classList.add('hidden');
+
+    } else {
+      document.getElementById('navConts').classList.add('hidden');
+      document.getElementById('menuButton').classList.add('flex');
     }
   }, [])
 
+  const handleOpenMenu = () => {
+    console.log('opening menu');
+  }
 
   return (
-    <motion.nav 
-      className='text-sm xs:text-sm md:text-md lg:text-xl flex py-2 px-4 bg-[#0d0d0d] fixed top-0 flex w-full justify-between z-10 text-gray-100 uppercase'
-      variants={slideDownAnimationVariants}
-      initial='initial'
-      whileInView='animate'
-      viewport={{
-        once: true,
-      }}
-    >
+    <motion.nav className='text-sm xs:text-sm md:text-md lg:text-xl flex py-2 px-4 bg-[#0d0d0d] fixed top-0 flex w-full justify-between z-10 text-gray-100 uppercase'>
       <ScrollLink to={'home'} smooth={true} className='transition ease-in-out duration-200 cursor-pointer hover:text-[#E0A526]'>
         <h1 className='p-1'>Marc Castro</h1>
       </ScrollLink>
-
+      <button
+        id='menuButton'
+        onClick={() => handleOpenMenu()}
+        className='uppercase absolute right-5 top-3 text-gray-100'
+      >
+        Menu
+      </button>
       <div id='navConts'>
         {contents.map((page) => {
           return (
